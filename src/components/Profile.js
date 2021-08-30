32e580b0d5bc18a84f81fd95dacaca729b34c751
Parvez Ahmed <miazyparvez@gmail.com>
2021-08-31 00:36:39 +0600

    Image gellary project using react and tailwind with unsplash api

---
+++ src/components/Profile.js
@@ -0,0 +1,29 @@
+import React from "react";
+import profile from "../assets/profile.jpg";
+export const Profile = () => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img className="w-full" src={profile} alt="Display" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Parvez Ahmed
+        </div>
+        <p className="text-gray-700 text-base">
+          When i’m not coding i switch to netflix with biscuits and cold tea as
+          my companion. <span></span>😜
+        </p>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #Software Engineer
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #Writter
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #Public Speaker
+        </span>
+      </div>
+    </div>
+  );
+};
